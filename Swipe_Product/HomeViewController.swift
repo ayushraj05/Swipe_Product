fba4be1c42644da0833cc05b8f6a713a838685e7
Ayush Rajpal <ayushrajpal2021@gmail.com>
2025-01-19 16:57:50 +0530

    Initial Commit

---
+++ Swipe_Product/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Swipe_Product
+//
+//  Created by Ayush Rajpal on 19/01/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ Swipe_Product/Swipe_ProductApp.swift
@@ -0,0 +1,20 @@
+//
+//  Swipe_ProductApp.swift
+//  Swipe_Product
+//
+//  Created by Ayush Rajpal on 19/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Swipe_ProductApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
